fd1fc5e3f4cbc8f7d3d0223adba5a04a161a6a78
Bob Hampton <132041117+bobhampton@users.noreply.github.com>
2025-10-14 13:32:12 -0600

    refactor: update LockTokens component to match dev

--- src/components/stream/LockTokens.tsx
+++ src/components/stream/LockTokens.tsx
@@ -90,12 +90,12 @@ export default function LockTokens({
 
   return (
     <div className="relative mx-auto rounded-[16px] shadow-lg border-b border-[#2C2C2C]" style={{ border: '0.62px solid #181818' }}>
-      <div className="relative z-10 h-full flex flex-col justify-between">
+      <div className={`relative z-10 h-full flex flex-col justify-between ${lockedBet ? 'pb-20' : ''}`}>
         <div>
           <div className="bg-[#242424] flex flex-col sm:flex-row sm:justify-between items-center rounded-t-2xl p-3 sm:p-5 px-[20px] sm:px-[55px] pr-[20px] sm:pr-[55px] gap-3 sm:gap-4 text-center">
             <div className="flex-shrink-0 w-full sm:w-auto">
               <p className="text-xs text-[#606060] font-semibold text-center pb-1">Your pick</p>
-              <p className="font-medium text-sm sm:text-[16px] text-[#D7DFEF]">{Number(localBetAmount)?.toLocaleString('en-US')} {updatedCurrency === CurrencyType.GOLD_COINS ? 'Gold Coins' : 'Sweep Coins'}</p>
+              <p className="font-medium text-sm sm:text-[16px] text-[#D7DFEF]">{Number(localBetAmount)?.toLocaleString('en-US')} {updatedCurrency === CurrencyType.GOLD_COINS ? 'Gold Coins' : 'Stream Coins'}</p>
             </div>
             <div className="flex-1 sm:min-w-0 w-full border-t sm:border-t-0 sm:border-1 border-[#2C2C2C] pt-3 sm:pt-0 sm:pl-4">
               <p className="text-xs text-[#606060] font-semibold text-center pb-1">Selected winner</p>
@@ -112,9 +112,6 @@ export default function LockTokens({
           	</p>
 		  </div>
         </div>
-        {lockedBet && (
-          <div className="mb-20"></div>
-        )}
         {!lockedBet && (
           <div className="flex justify-center gap-4 mb-8">
             <button 
