c0097571ade02072ec9043132f53b960b8088990
daryl-streambet <daryl@streambet.tv>
2025-11-07 19:24:44 +0800

    removed upcoming bets

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           </Button>
         </div> */}
         <HomeBets />
-        <UpcomingHomeBets />
+        {/* <UpcomingHomeBets /> */}
       </div>
     </MainLayout>
   );
