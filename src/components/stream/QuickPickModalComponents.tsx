2d5e7903977f656fb2ab6f9482c2b1fd5cc31107
Bob Hampton <132041117+bobhampton@users.noreply.github.com>
2025-11-10 17:51:49 -0700

    fix: update alt text for no picks image for better accessibility

--- src/components/stream/QuickPickModalComponents.tsx
+++ src/components/stream/QuickPickModalComponents.tsx
@@ -26,7 +26,7 @@ export const NoBettingData = () => {
       <div className="all-center flex justify-center items-center h-[100px] mt-8">
         <img
           src="/icons/nobettingData.svg"
-          alt="no picking data"
+          alt="no picks available"
           className="w-[100%] h-[100%] object-contain"
         />
       </div>
