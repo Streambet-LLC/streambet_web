e4b4ab33887e8233d8de74cc31b85f8bd66ba0e1
Bob Hampton <132041117+bobhampton@users.noreply.github.com>
2025-11-20 12:20:04 -0700

    Add Swiper carousel for featured bets and update styles

--- src/components/home/HomePromotedBets.tsx
+++ src/components/home/HomePromotedBets.tsx
@@ -1,11 +1,3 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselDots,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel';
 import BetCard from '../BetCard';
 import { useQuery } from '@tanstack/react-query';
 import api from '@/integrations/api/client';
@@ -16,6 +8,14 @@ import { PRIORITY_STREAMS } from '@/utils/constants';
 import { sortByPriorityPairs } from '@/utils/helper';
 import { BetCard as BetCardType } from '@/types/bet';
 import { useState, useMemo } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation, Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default function HomePromotedBets() {
   const [quickPickOpen, setQuickPickOpen] = useState(false);
@@ -47,40 +47,52 @@ export default function HomePromotedBets() {
   return (
     <>
       <div className="text-2xl font-bold pl-2">Featured Streams</div>
-      <Carousel className="flex-1 w-full">
-        <CarouselContent className="flex-1">
-          {isLoading ? (
-            <Skeleton className="flex-1 w-full h-64 rounded-none" />
-          ) : (
-            sortedData?.map((bet, i) => (
-              <CarouselItem key={i}>
-                <BetCard
-                  {...bet}
-                  setQuickPick={(streamId, roundId, streamName) => {
-                    setQuickPickModalSettings({
-                      streamId,
-                      streamName,
-                      roundId,
-                    });
-                    setQuickPickOpen(true);
-                  }}
-                />
-              </CarouselItem>
-            ))
-          )}
-        </CarouselContent>
-        <div className="flex items-center justify-between pt-4">
-          <CarouselPrevious
-            className="relative top-0 left-0 translate-y-[unset] translate-x-[unset]"
-            size="lg"
-          />
-          <CarouselDots className="relative" />
-          <CarouselNext
-            className="relative top-0 left-0 translate-y-[unset] translate-x-[unset]"
-            size="lg"
-          />
-        </div>
-      </Carousel>
+      {isLoading ? (
+        <Skeleton className="flex-1 w-full h-64 rounded-none" />
+      ) : (
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          autoplay={{
+            delay: 15000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          loop={true}
+          speed={600}
+          modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+          className="featured-bets-swiper"
+        >
+          {sortedData?.map((bet, i) => (
+            <SwiperSlide key={i}>
+              <BetCard
+                {...bet}
+                setQuickPick={(streamId, roundId, streamName) => {
+                  setQuickPickModalSettings({
+                    streamId,
+                    streamName,
+                    roundId,
+                  });
+                  setQuickPickOpen(true);
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
       {quickPickOpen && (
         <QuickPickModal
           open={quickPickOpen}
