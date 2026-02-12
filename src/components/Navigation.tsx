58a383920d4a444db57f7c5ee85561a3510ca5ac
aaron-streambet <aaron@streambet.tv>
2026-02-11 20:25:08 -0800

    adds shop coming soon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -116,6 +116,7 @@ export const Navigation = ({ onDashboardClick, searchValue, onSearchChange }: Na
     { label: 'Leaderboard', icon: undefined, path: '/leaderboard' },
     { label: 'Prizes', icon: undefined, path: '/prizes' },
     { label: 'How To Play', icon: undefined, path: '/how-to-play' },
+    { label: 'Shop', icon: undefined, path: '/shop' },
     (session?.role === 'admin' || session?.role === 'creator') && {
       label: session?.role === 'admin' ? 'Admin Dashboard' : 'Creator Dashboard',
       path: session?.role === 'admin' ? '/admin' : '/creator',
